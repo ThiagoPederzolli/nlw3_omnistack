259b46e9bb27bca235cc1cbc43ad7558e7ac3119
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-13 19:32:22 -0300

    iniciando criacao de tabela de imagens

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
-
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.post('/orphanages', (request, response) => {
-  
-  return response.json({ message: 'Hello World'});
-});
+
 
 app.listen(3333);
